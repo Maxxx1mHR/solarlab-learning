9976680385d1b06100f86a00729291ee18758474
Maksim Pudeev <maksim.pudeev@stm-labs.ru>
2025-09-13 12:25:27 +0300

    feat: Добавил primeng. Переделал header с использованием компонентов primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import {MyPreset} from './myPreset';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: MyPreset
+      }
+    })
   ]
 };
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
+import {ButtonModule} from 'primeng/button';
+import {FormsModule} from '@angular/forms';
+import {InputTextModule} from 'primeng/inputtext';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ButtonModule, FormsModule, InputTextModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   standalone: true
