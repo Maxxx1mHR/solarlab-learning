3ab60d8d16ffeaa01753477211c226b0ebe3e530
Maksim Pudeev <maksim.pudeev@stm-labs.ru>
2025-09-21 20:19:19 +0300

    feat: Добавил получение категорий

--- src/app/infrastructure/categories/services/categories.api.service.ts
+++ src/app/infrastructure/categories/services/categories.api.service.ts
@@ -1,13 +1,19 @@
-import {inject, Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {GetAllCategoryResponseDto} from '../dto';
-import {environment} from '@environments'
-import {GetCategoryResponseDto} from '../dto/category.get-by-id.dto';
-import {CreateCategoryRequestDto, CreateCategoryResponseDto} from '../dto/category.create.dto';
-import {UpdateCategoryByIdRequestDto, UpdateCategoryByIdResponseDto} from '../dto/category.update-by-id.dto';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GetAllCategoryResponseDto } from '../dto';
+import { environment } from '@environments';
+import { GetCategoryResponseDto } from '../dto/category.get-by-id.dto';
+import {
+  CreateCategoryRequestDto,
+  CreateCategoryResponseDto,
+} from '../dto/category.create.dto';
+import {
+  UpdateCategoryByIdRequestDto,
+  UpdateCategoryByIdResponseDto,
+} from '../dto/category.update-by-id.dto';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoriesApiService {
   private http = inject(HttpClient);
@@ -15,21 +21,29 @@ export class CategoriesApiService {
 
   getCategories = () => {
     return this.http.get<GetAllCategoryResponseDto>(`${this.base}/categories`);
-  }
+  };
 
   getCategory = (id: string) => {
-    return this.http.get<GetCategoryResponseDto>(`${this.base}/categories/${id}`)
-  }
+    return this.http.get<GetCategoryResponseDto>(
+      `${this.base}/categories/${id}`,
+    );
+  };
 
   createCategory = (data: CreateCategoryRequestDto) => {
-    return this.http.post<CreateCategoryResponseDto>(`${this.base}/categories`, {data})
-  }
+    return this.http.post<CreateCategoryResponseDto>(
+      `${this.base}/categories`,
+      { data },
+    );
+  };
 
   updateCategory = (id: string, data: UpdateCategoryByIdRequestDto) => {
-    return this.http.put<UpdateCategoryByIdResponseDto>(`${this.base}/categories/${id}`, {data});
-  }
+    return this.http.put<UpdateCategoryByIdResponseDto>(
+      `${this.base}/categories/${id}`,
+      { data },
+    );
+  };
 
   deleteCategory = (id: string) => {
-    return this.http.delete(`${this.base}/categories/${id}`)
-  }
+    return this.http.delete(`${this.base}/categories/${id}`);
+  };
 }
--- src/app/shared/components/smart/header/header.ts
+++ src/app/shared/components/smart/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal, ViewChild } from '@angular/core';
+import { Component, inject, signal, ViewChild, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
@@ -11,7 +11,15 @@ import { AuthorizationService, AuthorizationStateService } from '@core';
 import { UserStoreService } from '../../../../entries/users/user.store.service';
 import { InputGroup } from 'primeng/inputgroup';
 import { InputGroupAddon } from 'primeng/inputgroupaddon';
-
+import { MenuItem } from 'primeng/api';
+import { MegaMenu } from 'primeng/megamenu';
+import { TieredMenu } from 'primeng/tieredmenu';
+import { CategoriesService } from '../../../../entries/catergories/categories.service';
+interface Category {
+  id: string;
+  parentId: string;
+  name: string;
+}
 @Component({
   selector: 'app-header',
   imports: [
@@ -26,23 +34,61 @@ import { InputGroupAddon } from 'primeng/inputgroupaddon';
     Modal,
     InputGroup,
     InputGroupAddon,
+    MegaMenu,
+    TieredMenu,
   ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   standalone: true,
 })
-export class Header {
+export class Header implements OnInit {
   @ViewChild('popoverSettings') popoverSettings!: Popover;
   authMod = signal<'login' | 'register'>('login');
+  menuItem = signal<MenuItem[]>([]);
 
   authorizationSate = inject(AuthorizationStateService);
   authorizationService = inject(AuthorizationService);
+  categoriesService = inject(CategoriesService);
 
   userStore = inject(UserStoreService);
 
   isLoginModalOpen = false;
   isRegisterModalOpen = false;
 
+  items: MenuItem[] | undefined;
+
+  onCategoryClick(category: Category, fullPath: string) {
+    console.log(this.menuItem());
+    console.log('Выбрана категория:', category);
+    console.log('fullPath:', fullPath);
+  }
+
+  mapCategories(
+    categories: Category[],
+    parentId = '00000000-0000-0000-0000-000000000000',
+    parentPath = '',
+  ): MenuItem[] {
+    return categories
+      .filter((cat) => cat.parentId === parentId)
+      .map((c) => {
+        const fullPath = parentPath ? `${parentPath} / ${c.name}` : c.name;
+        const children = this.mapCategories(categories, c.id, fullPath);
+
+        return {
+          id: c.id,
+          label: c.name,
+          items: children.length > 0 ? children : undefined,
+          fullPath,
+          command: () => this.onCategoryClick(c, fullPath),
+        };
+      });
+  }
+
+  ngOnInit() {
+    this.categoriesService.getCategories().subscribe({
+      next: (categories) => this.menuItem.set(this.mapCategories(categories)),
+    });
+  }
   changeAuthMod() {
     this.authMod.update((value) => (value === 'login' ? 'register' : 'login'));
   }
